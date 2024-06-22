376c69c512b45d1be6a1b907a07873da8a993e23
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-06-22 17:38:41 +0300

    layout fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
     import "../app.css";
+    import Header from "$lib/components/Header.svelte";
+    import Navigation from "$lib/components/Navigation.svelte";
+    import Footer from "$lib/components/Footer.svelte";
 </script>
 
+<Header />
+<Navigation />
 <slot />
+<Footer />

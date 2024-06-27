4156fdf71b9785e4274dc9de8df77b870b7633f4
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-06-27 10:41:18 +0300

    nav in header🤯

--- front/src/lib/components/Header.svelte
+++ front/src/lib/components/Header.svelte
@@ -1,3 +1,25 @@
+<script lang="ts">
+    import Dropdown from "./Dropdown.svelte";
+
+    const puppies = [
+        {
+            href: "/",
+            text: "Bernese mountain dog puppies",
+            title: "Bernese mountain dog puppies",
+        },
+        {
+            href: "/",
+            text: "Entlebucher cattle dog puppies",
+            title: "Entlebucher cattle dog puppies",
+        },
+        {
+            href: "/",
+            text: "Australian shepherd puppies",
+            title: "Australian shepherd puppies",
+        },
+    ];
+</script>
+
 <header
     class="flex w-full flex-row items-center gap-8 p-2 md:px-44 lg:px-64 dark:bg-black"
 >
@@ -29,3 +51,24 @@
         </svg>
     </a>
 </header>
+<nav
+    class="flex flex-row gap-8 border-b border-b-black md:px-44 lg:px-64 dark:border-b-white dark:bg-black dark:text-white"
+>
+    <a
+        class="transition-colors duration-300 hover:text-slate-500"
+        href="/"
+        title="News"
+    >
+        News
+    </a>
+    <Dropdown items={puppies}>Puppies</Dropdown>
+    <Dropdown items={puppies}>Dogs</Dropdown>
+    <Dropdown items={puppies}>Litters</Dropdown>
+    <a
+        class="transition-colors duration-300 hover:text-slate-500"
+        href="/contact"
+        title="Contact page"
+    >
+        Contact
+    </a>
+</nav>

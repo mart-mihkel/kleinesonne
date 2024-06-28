8ae44f85cc5a1414da983efbc5157ce9576d7d36
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-06-28 22:57:08 +0300

    refactor⚙️

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,8 +1,62 @@
-<script lang="ts">
-    import Puppies from "$lib/components/Puppies.svelte";
-</script>
-
-<Puppies />
+<div class="dark:bg-black dark:text-white">
+    <h2>Puppies available</h2>
+    <div class="flex flex-col md:h-[600px] md:flex-row">
+        <div
+            class="group relative transition-all duration-500 ease-out md:w-1/3 hover:md:w-1/2"
+        >
+            <div
+                class="size-full brightness-50 transition-all duration-500 group-hover:brightness-100"
+            >
+                <img
+                    class="size-full object-cover"
+                    src="/usin.webp"
+                    alt="bernese"
+                />
+            </div>
+            <p
+                class="absolute bottom-1 left-1/2 -translate-x-1/2 text-center font-sans text-xl text-white transition-all duration-500 ease-out group-hover:text-2xl"
+            >
+                Bernese mountain dogs
+            </p>
+        </div>
+        <div
+            class="group relative transition-all duration-500 ease-out md:w-1/3 hover:md:w-1/2"
+        >
+            <div
+                class="size-full brightness-50 transition-all duration-500 group-hover:brightness-100"
+            >
+                <img
+                    class="size-full object-cover"
+                    src="/viisakas.webp"
+                    alt="entlebuch"
+                />
+            </div>
+            <p
+                class="absolute bottom-1 left-1/2 -translate-x-1/2 text-center font-sans text-xl text-white transition-all duration-500 ease-out group-hover:text-2xl"
+            >
+                Entlebucher cattle dogs
+            </p>
+        </div>
+        <div
+            class="group relative transition-all duration-500 ease-out md:w-1/3 hover:md:w-1/2"
+        >
+            <div
+                class="size-full brightness-50 transition-all duration-500 group-hover:brightness-100"
+            >
+                <img
+                    class="size-full object-cover"
+                    src="/rand.webp"
+                    alt="australian shepherd"
+                />
+            </div>
+            <p
+                class="absolute bottom-1 left-1/2 -translate-x-1/2 text-center font-sans text-xl text-white transition-all duration-500 ease-out group-hover:text-2xl"
+            >
+                Australian shpeherds
+            </p>
+        </div>
+    </div>
+</div>
 <div
     class="flex flex-col gap-4 py-4 md:px-44 lg:px-64 dark:bg-black dark:text-white"
 >

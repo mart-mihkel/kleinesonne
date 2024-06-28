c1400371c31b5fc7354a609d0883295a4c9a95ff
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-06-28 10:33:22 +0300

    refactor⚙️

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,7 +1,46 @@
 <script lang="ts">
     import Puppies from "$lib/components/Puppies.svelte";
-    import About from "$lib/components/About.svelte";
 </script>
 
 <Puppies />
-<About />
+<div
+    class="flex flex-col gap-4 py-4 md:px-44 lg:px-64 dark:bg-black dark:text-white"
+>
+    <h2>About</h2>
+    <p class="text-justify">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+        commodo leo aliquet massa viverra dapibus. Aliquam at viverra tortor.
+        Vestibulum eros sapien, tempor in elit a, faucibus vehicula ipsum.
+        Aliquam vel dapibus massa. Curabitur sed porta eros. Pellentesque quis
+        ligula vel risus congue commodo nec ut urna. Fusce ut nibh neque.
+        Aliquam ac purus facilisis, elementum felis luctus, tincidunt quam.
+        Nullam id diam a mi lobortis sodales aliquet et justo. Donec tristique
+        lectus sit amet porttitor pharetra. Proin a luctus velit. Mauris
+        malesuada, eros eu maximus ultricies, massa dolor hendrerit dui, id
+        malesuada turpis purus at tortor. Donec feugiat tempor ex vitae
+        pellentesque.
+    </p>
+    <p class="text-justify">
+        Ut non arcu in massa faucibus aliquam at nec risus. Fusce vulputate
+        lacinia aliquet. Proin viverra dolor mauris, at ultricies neque
+        ullamcorper nec. Fusce egestas mi eu nunc lacinia, imperdiet gravida
+        felis auctor. Vestibulum id dapibus elit, ac molestie risus. In hac
+        habitasse platea dictumst. Duis in maximus odio. Nam nec purus tellus.
+        Cras vulputate purus sed neque eleifend, eget finibus massa vehicula.
+        Nam eleifend metus nec erat volutpat vehicula. Integer id massa sit amet
+        sem sagittis ultrices. Proin a sodales massa, in dictum ex. Duis viverra
+        elit eu leo pellentesque tincidunt. Pellentesque a neque eget tortor
+        sagittis venenatis vitae vel odio. Maecenas sit amet ipsum eu orci
+        tristique pretium. Etiam dictum sagittis risus, non lacinia urna
+        vestibulum et.
+    </p>
+    <p class="text-justify">
+        Phasellus facilisis euismod lectus eu tincidunt. Aenean in diam ac
+        lectus molestie venenatis ac et sapien. Suspendisse justo elit,
+        venenatis eget consectetur vitae, condimentum ac risus. Nullam ut dolor
+        est. Cras placerat ultricies libero mollis sollicitudin. Quisque vel
+        velit est. Pellentesque nisl velit, varius vel euismod quis, tempor vel
+        ex. Nulla ut faucibus nisi. Phasellus pharetra arcu odio, at rutrum eros
+        auctor ac.
+    </p>
+</div>

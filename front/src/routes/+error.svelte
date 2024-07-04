b33ed0eaba3a3c61399e1f8432e3ada7f079070c
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-07-04 21:01:13 +0300

    error page❗

---
+++ front/src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { Error } from "$lib/components";
+    import { page } from "$app/stores";
+</script>
+
+<div
+    class="flex flex-col items-center justify-center gap-4 py-4 md:px-[5%] lg:px-[25%]"
+>
+    {#if $page.error}
+        <Error message={`Error ${$page.status}: ${$page.error.message}`} />
+    {/if}
+</div>

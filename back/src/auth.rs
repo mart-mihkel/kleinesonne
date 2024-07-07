d03f4f637605cf3bb197db82e5e60f224157373b
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-07-07 19:21:04 +0300

    authorization skeleton💀

--- back/src/auth.rs
+++ back/src/auth.rs
@@ -1,17 +1,49 @@
+use axum::{
+    async_trait,
+    extract::FromRequestParts,
+    http::{request::Parts, StatusCode},
+    response::{IntoResponse, Response},
+    Json, RequestPartsExt,
+};
+use axum_extra::{
+    headers::{authorization::Bearer, Authorization},
+    TypedHeader,
+};
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
+use serde::{Deserialize, Serialize};
 use std::time::Duration;
 
-use axum::{
-    http::StatusCode,
-    response::{IntoResponse, Response},
-    Json,
-};
-use jsonwebtoken::{encode, EncodingKey, Header};
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 struct Claims {
     sub: String,
     exp: usize,
+}
+
+#[async_trait]
+impl<S> FromRequestParts<S> for Claims
+where
+    S: Send + Sync,
+{
+    type Rejection = AuthError;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        let TypedHeader(Authorization(bearer)) = parts
+            .extract::<TypedHeader<Authorization<Bearer>>>()
+            .await
+            .map_err(|_| AuthError::InvalidToken)?;
+
+        let secret =
+            std::env::var("JWT_SECRET").expect("Environment variable JWT_SECRET is not defined");
+
+        let token_data = decode::<Claims>(
+            bearer.token(),
+            &DecodingKey::from_secret(secret.as_bytes()),
+            &Validation::default(),
+        )
+        .map_err(|_| AuthError::InvalidToken)?;
+
+        Ok(token_data.claims)
+    }
 }
 
 #[derive(Deserialize)]
@@ -35,13 +67,23 @@ pub enum AuthError {
 
 impl IntoResponse for AuthError {
     fn into_response(self) -> Response {
-        match self {
+        #[derive(Serialize)]
+        struct ErrorMessage {
+            error: String,
+        }
+
+        let (status, message) = match self {
             AuthError::WrongCredentials => (StatusCode::UNAUTHORIZED, "Wrong credentials"),
             AuthError::MissingCredentials => (StatusCode::BAD_REQUEST, "Missing credentials"),
             AuthError::TokenCreation => (StatusCode::INTERNAL_SERVER_ERROR, "Token creation error"),
             AuthError::InvalidToken => (StatusCode::BAD_REQUEST, "Invalid token"),
-        }
-        .into_response()
+        };
+
+        let body = Json(ErrorMessage {
+            error: message.into(),
+        });
+
+        (status, body).into_response()
     }
 }
 
@@ -58,11 +100,11 @@ pub async fn login(Json(payload): Json<AuthPayload>) -> Result<Json<AuthResponse
 
     let claims = Claims {
         sub: username,
-        exp: get_exp(6000),
+        exp: get_exp(3600),
     };
 
     let secret =
-        std::env::var("JWT_SECRET").expect("Environment variable JWT_SECRET is not define");
+        std::env::var("JWT_SECRET").expect("Environment variable JWT_SECRET is not defined");
 
     let token = encode(
         &Header::default(),
@@ -79,5 +121,5 @@ fn get_exp(seconds: u64) -> usize {
         .duration_since(std::time::UNIX_EPOCH)
         .expect("Time went backwards");
 
-    (now + Duration::from_secs(seconds)).as_millis() as usize
+    (now + Duration::from_secs(seconds)).as_secs() as usize
 }

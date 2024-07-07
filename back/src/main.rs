9c13d9948ef8f8c66bad3cb7ca96cb54a6170894
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-07-07 19:21:21 +0300

    enpoints skeleton💀

--- back/src/main.rs
+++ back/src/main.rs
@@ -7,7 +7,24 @@ mod auth;
 
 #[tokio::main]
 async fn main() {
-    let api = Router::new().route("/admin/login", post(auth::login));
+    let api = Router::new()
+        .route("/authenticate", post(auth::login))
+        .route("/news/create", post(|| async { "hi" }))
+        .route("/news/read", get(|| async { "hi" }))
+        .route("/news/update", post(|| async { "hi" }))
+        .route("/news/delete", post(|| async { "hi" }))
+        .route("/dogs/create", post(|| async { "hi" }))
+        .route("/dogs/read", get(|| async { "hi" }))
+        .route("/dogs/update", post(|| async { "hi" }))
+        .route("/dogs/delete", post(|| async { "hi" }))
+        .route("/litters/create", post(|| async { "hi" }))
+        .route("/litters/read", get(|| async { "hi" }))
+        .route("/litters/update", post(|| async { "hi" }))
+        .route("/litters/delete", post(|| async { "hi" }))
+        .route("/puppies/create", post(|| async { "hi" }))
+        .route("/puppies/read", get(|| async { "hi" }))
+        .route("/puppies/update", post(|| async { "hi" }))
+        .route("/puppies/delete", post(|| async { "hi" }));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await

9c221f89d81ea4aadb5b48e54707f6f251ffd774
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-07-09 22:40:09 +0300

    connect🔗

--- crates/db/src/lib.rs
+++ crates/db/src/lib.rs
@@ -6,3 +6,19 @@ pub use cornucopia::queries::litter;
 pub use cornucopia::queries::news;
 pub use cornucopia::queries::puppy;
 pub use cornucopia::types::public::{Availability, Breed, Gender};
+
+pub use tokio_postgres::Client;
+use tokio_postgres::NoTls;
+
+pub async fn connect() -> Result<Client, tokio_postgres::Error> {
+    let (client, connection) =
+        tokio_postgres::connect("host=localhost user=postgres", NoTls).await?;
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("Database connection error: {}", e);
+        }
+    });
+
+    Ok(client)
+}

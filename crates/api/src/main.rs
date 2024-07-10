bf5a219fb84a4fdc5f83bf5da2c2e3243f564f53
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-07-10 08:16:37 +0300

    api initial✨

---
+++ crates/api/src/main.rs
@@ -0,0 +1,19 @@
+use std::{
+    cell::Cell,
+    sync::{Arc, Mutex},
+};
+
+use axum::{serve, Extension, Router};
+use db::Client;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let client = Arc::new(Cell::new(db::connect().await.unwrap()));
+
+    let routes = Router::new().layer(Extension(client));
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+
+    serve(listener, routes).await.unwrap();
+}

ff0a18f43507add25605e5eff6e74a6f039bdc45
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-07-10 15:33:54 +0300

    new dog🐶

--- crates/api/src/dog/mod.rs
+++ crates/api/src/dog/mod.rs
@@ -7,4 +7,5 @@ pub fn routes() -> Router {
     Router::new()
         .route("/dog/id", post(read::dog_by_id))
         .route("/dogs/breed", post(read::dogs_by_breed_and_status))
+        .route("/dogs/new", post(new::new_dog))
 }
--- crates/api/src/dog/new.rs
+++ crates/api/src/dog/new.rs
@@ -1,1 +1,60 @@
-fn new_dog() {}
+use std::sync::Arc;
+
+use axum::{response::IntoResponse, Extension, Json};
+use serde::Deserialize;
+use tokio::sync::Mutex;
+
+use crate::{errors::ApiError, util::de_primitive};
+
+#[derive(Deserialize)]
+pub struct NewDog {
+    name: String,
+    nickname: String,
+    father: String,
+    mother: String,
+    #[serde(deserialize_with = "de_primitive")]
+    dob: i64,
+    #[serde(with = "db::BreedDef")]
+    breed: db::Breed,
+    #[serde(with = "db::GenderDef")]
+    gender: db::Gender,
+    #[serde(deserialize_with = "de_primitive")]
+    alive: bool,
+    thumbnail: String,
+    health: Vec<String>,
+    titles: Vec<String>,
+    images: Vec<String>,
+}
+
+pub async fn new_dog(
+    Extension(client): Extension<Arc<Mutex<db::Client>>>,
+    Json(dog): Json<NewDog>,
+) -> Result<impl IntoResponse, ApiError> {
+    let mut client = client.lock().await;
+    let tx = client
+        .transaction()
+        .await
+        .map_err(|_| ApiError::DatabaseError)?;
+
+    db::dog::insert_dog()
+        .bind(
+            &tx,
+            &dog.name,
+            &dog.nickname,
+            &dog.father,
+            &dog.mother,
+            &dog.breed,
+            &dog.gender,
+            &dog.dob,
+            &dog.alive,
+            &dog.thumbnail,
+            &dog.images,
+            &dog.health,
+            &dog.titles,
+        )
+        .await
+        .map_err(|_| ApiError::DatabaseError)?;
+
+    // TODO: impl
+    Ok(())
+}

d48e69f0fb3ba78209ffa023ed0a1b1148aeec02
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-07-10 10:13:37 +0300

    automaticjwtn validation⚙️

--- crates/api/src/admin/jwt.rs
+++ crates/api/src/admin/jwt.rs
@@ -1,10 +1,41 @@
+use std::time::{Duration, SystemTime};
+
+use axum::{extract::FromRequestParts, http::request::Parts, RequestPartsExt};
+use axum_extra::{
+    headers::{authorization::Bearer, Authorization},
+    TypedHeader,
+};
 use jsonwebtoken::{decode, encode, DecodingKey, Header, Validation};
+use serde::Deserialize;
 
 use crate::errors::ApiError;
 
-struct Claims {
+#[derive(Deserialize)]
+pub struct Claims {
     sub: String,
     exp: u64,
+}
+
+#[async_trait]
+impl<S> FromRequestParts<S> for Claims
+where
+    S: Send + Sync,
+{
+    type Rejection = ApiError;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        let secret = std::env::var("JWT_SECRET").map_err(|_| ApiError::Internal)?;
+
+        let TypedHeader(Authorization(bearer)) = parts
+            .extract::<TypedHeader<Authorization<Bearer>>>()
+            .await
+            .map_err(|_| ApiError::InvalidToken)?;
+
+        let token_data = decode::<Claims>(bearer.token(), &secret, &Validation::default())
+            .map_err(|_| ApiError::InvalidToken)?;
+
+        Ok(token_data.claims)
+    }
 }
 
 pub fn create_token(sub: String) -> Result<String, ApiError> {
@@ -18,19 +49,10 @@ pub fn create_token(sub: String) -> Result<String, ApiError> {
     Ok(token)
 }
 
-pub fn validate_token(token: String) -> Result<(), ApiError> {
-    let secret = std::env::var("JWT_SECRET").map_err(|_| ApiError::Internal)?;
-    let key = DecodingKey::from_secret(secret);
-
-    decode(&token, &key, &Validation::default()).map_err(|_| ApiError::InvalidToken)?;
-
-    Ok(())
-}
-
-fn secs_from_now(seconds: u64) -> u64 {
-    let now = std::time::SystemTime::now()
+fn secs_from_now(secs: u64) -> u64 {
+    let now = SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
         .expect("Time went backwards");
 
-    (now + Duration::from_secs(seconds)).as_secs()
+    (now + Duration::from_secs(secs)).as_secs()
 }

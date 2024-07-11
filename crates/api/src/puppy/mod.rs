a5838884566a19f4873f78e5b30c7878e325d1c0
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-07-11 22:18:08 +0300

    typo✏️

--- crates/api/src/puppy/mod.rs
+++ crates/api/src/puppy/mod.rs
@@ -13,6 +13,6 @@ pub fn routes() -> Router {
         .route("/puppy/names", post(read::names_by_litter))
         .route("/puppy/litter", post(read::puppies_by_litter))
         .route("/puppy/new", put(new::new_puppy))
-        .route("/puppy/modify", put(update::update_puppy))
+        .route("/puppy/update", put(update::update_puppy))
         .route("/puppy/delete", delete(delete::delete_puppy))
 }

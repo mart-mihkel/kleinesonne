bb75412304eca084b5984c54c11f016b8ab651ab
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-07-21 15:18:24 +0300

    no csrf for dev mode🤮

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,11 +1,16 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import adapter from "@sveltejs/adapter-node";
 
+const IS_PROD = process.env.NODE_ENV === "production";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
     kit: {
         adapter: adapter(),
+        csrf: {
+            checkOrigin: IS_PROD,
+        },
     },
 };
 
